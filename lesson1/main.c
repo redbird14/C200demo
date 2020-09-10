62f18d4feeac4d7aef31ee38defbc4dddb210832
redbird14 <redbird14@163.com>
2020-09-10 17:31:00 +0800

    second commit

--- lesson1/main.c
+++ lesson1/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int i,j,n,m;
+    int a[16] =
+
+    {
+        0
+    };
+
+    printf("请输入一个整数（0~32767）：\n");
+    scanf("%d",&n);
+
+    for(m = 0; m < 15; m++)
+
+    {
+        i = n % 2;
+        j = n / 2;
+        n = j;
+        a[m] = i;
+    }
+
+    for(m = 15; m >= 0; m--)
+
+    {
+        printf("%d", a[m]);
+        if(m % 4 == 0)
+            printf(" ");
+    }
 }
